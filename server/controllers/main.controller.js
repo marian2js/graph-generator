a162f39b89a14f7978fa2b5756cd9af86cf29300
esfernandez <esfernandez.ef@gmail.com>
2016-06-22 22:11:38 -0300

    Funcionalidad de borrado de nodo (Controller)

--- server/controllers/main.controller.js
+++ server/controllers/main.controller.js
@@ -1,11 +1,7 @@
-const nodecontroller = require('./node.controller');
-
 class MainController {
 
   index(req, res) {
-    //res.render('index', { title: 'Graph Generator' });
-
-    nodecontroller.getNodes(req, res);
+    res.render('index', { title: 'Graph Generator' });
   }
 
 }
